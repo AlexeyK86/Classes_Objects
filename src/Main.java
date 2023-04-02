288b754175450ed340fea8daeb0a924c51c579e1
Алексей <alesha1986@bk.ru>
2023-04-02 19:03:43 +0300

    Classes and objects

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author pushkin = new Author ("Александр", "Пушкин");
+        Author blok = new Author("Александр", "Блок");
+
+        Book ruslanLudmila = new Book("Руслан и Людмила", pushkin, 1820);
+        Book stranger = new Book("Незнакомка", blok, 1906);
+
+        System.out.println("Заголовок книги: " + ruslanLudmila.getTitle());
+        System.out.println("Год издания книги: " + ruslanLudmila.getPublishingYear() );
+
+        ruslanLudmila.setPublishingYear(1821);
+
+        System.out.println("Год переиздания книги: " + ruslanLudmila.getPublishingYear());
+
+        System.out.println("Заголовок книги: " + stranger.getTitle());
+        System.out.println("Год издания книги: " + stranger.getPublishingYear() );
     }
 }
